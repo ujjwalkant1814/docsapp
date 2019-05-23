b4dc36faaf5e36660b6d19048de8b08d9a7a3588
ujjwalkant1814 <35636087+ujjwalkant1814@users.noreply.github.com>
2019-05-24 02:00:40 +0530

    Completed file imports

--- Topic_detection.py
+++ Topic_detection.py
@@ -7,8 +7,25 @@
 import pandas as pd
 import glob
 
+#Importing Data
 files = glob.glob("Health-Tweets/*.txt")
 
+data = pd.DataFrame({})
 for i in files:
-    print(i)
-    data = pd.read_csv('Health-Tweets\cbchealth.txt', sep ="|", header =  None)
+    try:
+        data_i = pd.read_csv(i, 
+                             sep ="|", 
+                             header =  None,
+                             error_bad_lines = False)
+    except UnicodeDecodeError:
+        print("Using ISO-8859-1 encoding for %s"%(i))
+        data_i = pd.read_csv(i, 
+                             sep ="|", 
+                             header =  None,
+                             error_bad_lines = False,
+                             encoding = "ISO-8859-1")
+    data = pd.concat([data, data_i], axis = 0)
+ 
+#lets rename the columns of the data
+data.columns = ['ID','Date','Comment']
+    
